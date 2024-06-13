728c5a91f0dee312fdc168d2e31f55415592c792
Jay <jay0816@outlook.com>
2024-06-13 18:29:32 +0200

    zwischencommit

--- Implementation/src/run_mcmc/run_dream.py
+++ Implementation/src/run_mcmc/run_dream.py
@@ -1,13 +1,17 @@
 import tensorflow_probability as tfp
 import numpy as np
+import pandas as pd
 import sys
 
-sys.path.append('/Users/jay/Desktop/Bachelorarbeit/Implementation/src')
-from execute_model import run_model_single_parameter_node
+sys.path.append('/Users/jay/Desktop/Bachelorarbeit/Implementation/')
+from src.execute_model import run_model_single_parameter_node
 from dependencies.PyDREAM.pydream.parameters import SampledParam
-from likelihood.likelihood_independent import likelihood_independent
+from src.likelihood.likelihood_independent import likelihood_independent
+from src.likelihood.likelihood_dependent import likelihood_dependent
 from dependencies.PyDREAM.pydream.core import run_dream
 from src.construct_model import get_model
+
+posterior_rudimentary = pd.read_csv('/Users/jay/Desktop/Bachelorarbeit/Implementation/src/run_mcmc/posterior_rudimentary.csv').apply(pd.to_numeric, errors='coerce')
 
 configPath = "/Users/jay/Desktop/Bachelorarbeit/Implementation/configurations/config_train_oldman.json"
 basis = "Oldman_Basin"
@@ -15,13 +19,54 @@ basis = "Oldman_Basin"
 model = get_model(configPath, basis)
 
 # Define Likelihood
-def likelihood_kernel(param_vec):
+def default_likelihood_kernel(param_vec):
     likelihood_function = likelihood_independent
     _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
     return likelihood_function(y_model, y_observed)
 
+def likelihood_d2(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_dependent(y_model, y_observed, sd=0.2)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
 
-def run_mcmc_dream():
+def likelihood_d4(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_dependent(y_model, y_observed, sd=0.4)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_d6(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_dependent(y_model, y_observed, sd=0.6)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_d8(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_dependent(y_model, y_observed, sd=0.8)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_i1(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_independent(y_model, y_observed, sd=1)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_i3(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_independent(y_model, y_observed, sd=3)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_i5(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_independent(y_model, y_observed, sd=5)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+def likelihood_i8(param_vec):
+    likelihood_function = lambda y_model, y_observed: likelihood_independent(y_model, y_observed, sd=8)
+    _, y_model, y_observed, _ = run_model_single_parameter_node(model, param_vec)
+    return likelihood_function(y_model, y_observed)
+
+
+
+def run_mcmc_dream(niterations=1250, nchains=8, **kwargs):
     # Construct Params
     configurationObject = model.configurationObject
     param_names = []
@@ -38,12 +83,76 @@ def run_mcmc_dream():
     param_lower = np.array(param_lower)
     param_upper = np.array(param_upper)
 
+    # Parameter Assignment
+    DEpairs = kwargs['DEpairs'] if 'DEpairs' in kwargs else 1
+    multitry = kwargs['multitry'] if 'multitry' in kwargs else False
+    hardboundaries = kwargs['hardboundaries'] if 'hardboundaries' in kwargs else True
+    crossover_burnin = kwargs['crossover_burnin'] if 'crossover_burnin' in kwargs else 0
+    nCR = kwargs['nCR'] if 'nCR' in kwargs else 3
+    snooker = kwargs['snooker'] if 'snooker' in kwargs else 0
+    p_gamma_unity = kwargs['p_gamma_unity'] if 'p_gamma_unity' in kwargs else 0
+    init_method = kwargs['init_method'] if 'init_method' in kwargs else 'not specified'
+
+    l_k = default_likelihood_kernel
+    if 'likelihood_dependence' in kwargs:
+        if 'likelihood_dependence' not in kwargs:
+            return 'Failure'
+
+        if kwargs['likelihood_dependence']:
+            if kwargs['likelihood_sd'] == 0.2:
+                l_k = likelihood_d2
+            elif kwargs['likelihood_sd'] == 0.4:
+                l_k = likelihood_d4
+            elif kwargs['likelihood_sd'] == 0.6:
+                l_k = likelihood_d6
+            elif kwargs['likelihood_sd'] == 0.8:
+                l_k = likelihood_d8
+        else:
+            if kwargs['likelihood_sd'] == 1:
+                l_k = likelihood_i1
+            elif kwargs['likelihood_sd'] == 3:
+                l_k = likelihood_i3
+            elif kwargs['likelihood_sd'] == 5:
+                l_k = likelihood_i5
+            elif kwargs['likelihood_sd'] == 8:
+                l_k = likelihood_i8
+
+
+    randomStart = False
+    # Initial state
+    if init_method == 'random' or init_method == 'not specified':
+        state = None
+        randomStart = True
+    elif init_method == 'min':
+        state = param_lower
+    elif init_method == 'max':
+        state = param_upper
+    elif init_method == 'q1_prior':
+        state = param_lower + ((param_upper - param_lower) / 4)
+    elif init_method == 'mean_prior':
+        state = (param_upper - param_lower) / 2
+    elif init_method == 'q3_prior':
+        state = param_lower + ((param_upper - param_lower) * 3 / 4)
+    elif init_method == 'q1_posterior':
+        state = np.array(posterior_rudimentary.iloc[1].values[1:])
+    elif init_method == 'median_posterior':
+        state = np.array(posterior_rudimentary.iloc[2].values[1:])
+    elif init_method == 'q3_posterior':
+        state = np.array(posterior_rudimentary.iloc[3].values[1:])
+
+    states = []
+    for _ in range(nchains):
+        states.append(state)
+
     # Run
-    niterations = 10000
-    nchains = 5
     parameters_to_sample = SampledParam(tfp.distributions.Uniform, low=param_lower, high=param_upper)
-    #The run_dream function expects a list rather than a single variable
     sampled_parameter = [parameters_to_sample]
-    sampled_params, log_ps = run_dream(sampled_parameter, likelihood_kernel, niterations=niterations, nchains=nchains, multitry=False, gamma_levels=4, adapt_gamma=True, history_thin=1, model_name='test_mcmc_hydrological', verbose=True)
+
+    sampled_params, _ = run_dream(sampled_parameter, l_k, \
+                                       niterations=niterations, nchains=nchains, \
+                                       start=states, DEpairs=DEpairs, multitry=multitry, \
+                                       hardboundaries=hardboundaries, crossover_burnin=crossover_burnin, \
+                                       nCR=nCR, snooker=snooker, p_gamma_unity=p_gamma_unity, randomStart=randomStart)
+    
     return sampled_params, niterations
 
--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/8_chain.py
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/5_chain.py
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/4_chain.py
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/2_chain.py
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/1_chain.py
-/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_parallel_mh/10_chain.py
+/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_dream/4_chain.py
+/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_dream/5_chain.py
+/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_dream/8_chain.py
+/Users/jay/miniconda3/envs/hbv_uq_env/bin/python /Users/jay/Desktop/Bachelorarbeit/Implementation/src/benchmark/benchmark_dream/10_chain.py
